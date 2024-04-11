07f0c4a3433512a11c6d349fb8a1e88b3fd889f6
RustaMSHar <rustam.lika@mail.ru>
2024-04-12 01:44:19 +0300

    Добавьте файлы проекта.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MapsDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
