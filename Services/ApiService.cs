ff097474ebabe4155743ff940ec0ff824a0e7340
RustaMSHar <rustam.lika@mail.ru>
2024-04-13 22:19:22 +0300

    some shit

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -9,15 +9,15 @@ using System.Threading.Tasks;
 
 namespace MapsDemo.Services
 {
-    public class ApiService
+    public static class ApiService
     {
         
-        public async void GetTracker()
+        public static async Task<Root> GetTracker()
         {
             var httpClient = new HttpClient();
             var ApiKey = "2d83a2-a21b99";
             var response = await httpClient.GetStringAsync(string.Format("https://aviation-edge.com/v2/public/flights?key={0}&limit=10", ApiKey));
-            JsonConvert.DeserializeObject<Root>(response);
+            return JsonConvert.DeserializeObject<Root>(response);
         }
     }
 }
