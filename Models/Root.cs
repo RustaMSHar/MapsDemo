4902a4a99d2813027e4341d81c632196e1b6ff7b
RustaMSHar <rustam.lika@mail.ru>
2024-04-13 21:16:59 +0300

    add apiservices

---
+++ Models/Root.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapsDemo.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Aircraft
+    {
+        public string iataCode { get; set; }
+        public string icao24 { get; set; }
+        public string icaoCode { get; set; }
+        public string regNumber { get; set; }
+    }
+
+    public class Airline
+    {
+        public string iataCode { get; set; }
+        public string icaoCode { get; set; }
+    }
+
+    public class Arrival
+    {
+        public string iataCode { get; set; }
+        public string icaoCode { get; set; }
+    }
+
+    public class Departure
+    {
+        public string iataCode { get; set; }
+        public string icaoCode { get; set; }
+    }
+
+    public class Flight
+    {
+        public string iataNumber { get; set; }
+        public string icaoNumber { get; set; }
+        public string number { get; set; }
+    }
+
+    public class Geography
+    {
+        public int altitude { get; set; }
+        public int direction { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class Root
+    {
+        public Aircraft aircraft { get; set; }
+        public Airline airline { get; set; }
+        public Arrival arrival { get; set; }
+        public Departure departure { get; set; }
+        public Flight flight { get; set; }
+        public Geography geography { get; set; }
+        public Speed speed { get; set; }
+        public string status { get; set; }
+        public System system { get; set; }
+    }
+
+    public class Speed
+    {
+        public double horizontal { get; set; }
+        public int isGround { get; set; }
+        public int vspeed { get; set; }
+    }
+
+    public class System
+    {
+        public object squawk { get; set; }
+        public int updated { get; set; }
+    }
+}
---
+++ Services/ApiService.cs
@@ -0,0 +1,23 @@
+﻿using MapsDemo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace MapsDemo.Services
+{
+    public class ApiService
+    {
+        
+        public async void GetTracker()
+        {
+            var httpClient = new HttpClient();
+            var ApiKey = "2d83a2-a21b99";
+            var response = await httpClient.GetStringAsync(string.Format("https://aviation-edge.com/v2/public/flights?key={0}&limit=10", ApiKey));
+            JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
