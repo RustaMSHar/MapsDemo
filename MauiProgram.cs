2844c43fd29a7538358f266a0fcf144abe939448
RustaMSHar <rustam.lika@mail.ru>
2024-04-29 21:26:19 +0300

    some changes

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Maps;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Maps;
 
 namespace MapsDemo;
 
@@ -16,10 +19,10 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-#if DEBUG
+#if DEBUG || WINDOWS
         builder.Logging.AddDebug();
-#endif
-
+        builder.UseMauiCommunityToolkitMaps("AmO8YdGUtzObHvwdz_m1zwuL_ZbHYYZYu2S0F6lR80Gz3VTkoKFAt254DJT1LUeZ");
+#endif  
 
 
         return builder.Build();
