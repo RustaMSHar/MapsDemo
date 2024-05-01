423d46829f48b756fe5de15b26f0541ef6a5fac0
RustaMSHar <rustam.lika@mail.ru>
2024-05-01 23:57:45 +0300

    some

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,14 +1,39 @@
-﻿namespace MapsDemo
+﻿using Microsoft.Maui.Controls;
+using MapsDemo.Models;
+using MapsDemo.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MapsDemo
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+       
 
         public MainPage()
         {
             InitializeComponent();
+            LoadFlightData();
         }
 
+
+        private async void LoadFlightData()
+        {
+            var flightData = await ApiService.GetFlightData();
+            if (flightData != null && flightData.geography != null)
+            {
+                foreach (var geo in flightData.geography)
+                {
+                    var position = new Position(geo.latitude, geo.longitude);
+                    var marker = new Marker
+                    {
+                        Position = position,
+                        Label = "Aircraft"
+                    };
+                    map.AddMarker(marker);
+                }
+            }
+        }
 
     }
 
