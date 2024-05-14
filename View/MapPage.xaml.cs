8c716289209b74db61f0f2d2e5f1cc8d148957d3
RustaMSHar <rustam.lika@mail.ru>
2024-05-14 22:38:50 +0300

    add function to update flighttracker every 5 second

--- View/MapPage.xaml.cs
+++ View/MapPage.xaml.cs
@@ -6,21 +6,38 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+
 namespace MapsDemo
 {
     public partial class MainPage : ContentPage
     {
+       
+        
+
         public MainPage()
         {
             InitializeComponent();
-            LoadFlights();
+
+            LoadFlightsRepeatedly();
+
+        }
+        private async Task LoadFlightsRepeatedly()
+        {
+            while (true)
+            {
+                await LoadFlights();
+                await Task.Delay(5000); // Приостановка выполнения на 5 секунд
+            }
         }
 
-        private async void LoadFlights()
+        private async Task LoadFlights()
         {
             try
             {
                 var flights = await ApiService.GetTracker();
+                // Очистка текущих маркеров на карте
+                MyMap.Pins.Clear();
+
                 foreach (var flight in flights)
                 {
                     if (flight.Geography.Latitude != 0 && flight.Geography.Longitude != 0)
@@ -31,7 +48,6 @@ namespace MapsDemo
                             Address = flight.FlightDetails.IcaoNumber,
                             Type = PinType.Place,
                             Location = new Location(flight.Geography.Latitude, flight.Geography.Longitude)
-
                         };
                         MyMap.Pins.Add(pin);
                     }
