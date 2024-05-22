ce050451af217bb97bce0d700bfc4dfa3973669a
RustaMSHar <rustam.lika@mail.ru>
2024-05-23 00:26:36 +0300

    add notes

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace MapsDemo
+﻿using MapsDemo.View;
+
+namespace MapsDemo
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(NotePage), typeof(NotePage));
         }
     }
 }
